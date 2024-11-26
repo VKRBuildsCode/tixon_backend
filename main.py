84faca5779c79bba40bea6f9235daddca2369b83
9kalyan <rakalyanram@gmail.com>
2024-11-26 21:00:51 +0530

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+
+origins = [
+  "*"
+    # Add more allowed origins as needed
+]
+url = 'https://zohoapis.in/backstage/v3/portals/60034569483/events'
+
+headers = {
+    'Authorization': 'Zoho-oauthtoken 1000.303564e11510ada4e4ec00a71a274b0e.af6ce52cc6c176f4b07c9d6b9e7a00e4',  # Replace with your token
+}
+
+# Make the GET request with headers
+response = requests.get(url, headers=headers)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Parse JSON response if needed
+    data = response.json()
+    print("Response Data:", data['events'][0])
+else:
+    print(f"Error: {response.status_code} - {response.text}")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # List of allowed origins
+    allow_credentials=True,  # Allow credentials (cookies, authorization headers)
+    allow_methods=["*"],     # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],     # Allow all headers
+)
+@app.get("/")
+async def main():
+    data = response.json()
+    print(str(data))
+    return data['events']
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
+
+
+
+
+
+
